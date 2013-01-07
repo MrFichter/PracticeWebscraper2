6b99ae6f518527f0709b1f336448e1fc89df10bc
Jonathan Fichter <jonathan.fichter@live.com>
2013-01-07 06:03:58 -0500

    Moved old comments to new text file. I'll keep Next Steps and "Things that will help with Step ___" in main.py, but I moved older explanations of decisions I made to commentary.txt. Moving forward, I will rely more heavily on the git log to do things like paste in the stackOverflow URLs of sites that explain my decisions.

--- main.py
+++ main.py
@@ -44,36 +44,8 @@ content = page.read() ## Some people on the #python freenode irc \
 import re
 for term in SEARCHTERMS:
     termResults = re.findall ('(?i)' + term + '.*' , content) ## '(?i)' \
-##makes it case insensitive. It needs to come at the beginning, though.
-## I learned about using '(?i)' or other (?iLmsux) terms from here:
-##http://stackoverflow.com/questions/500864/case-insensitive-python-regular-expression-without-re-compile
-##(See answer from aem999.)
-##Aem999 uses and r before the expression, which treats the string a a raw
-##string and would ignore the symbolism of things like \n, as explained here:
-##http://stackoverflow.com/questions/4780088/in-python-what-does-preceding-a-string-literal-with-r-mean
-##Note: <bob_f> from the python irc recommended an altnerative strategy:
-##MrFichter: Yes, create your regex using \
-##re.compile and pass flags=re.IGNORECASE
-## See @Roger Pate's comments about re.compile here: http://stackoverflow.com/questions/452104/is-it-worth-using-pythons-re-compile
-
-##The line above is inspired by a line @Fichtitious
-##mentioned in a tutorial email he sent me:
-##re.findall('Published on.*', content)
-##@Fichtitious explained the following:
-##''.' is the wildcard character, and '*' means 0 or
-##more occurrences of the preceding character.  So
-##'.*' means go match 0 or more of anything, and
-##'Published on.*' means go match 'Published on'
-##followed by 0 or more of anything.
-##By default, all re matches go only as far as
-##the end of the line, so this time we matched each
-##occurrence of 'Published on' and all the
-##characters coming after it until the next newline.'
-##I wasn't sure if I'd be able to concatenate
-##'.*' with my variable 'term,' but it looks like
-##this worked!
-
-##re.findall () returns results at a list
+##makes it case insensitive. It needs to come at the beginning.
+##re.findall () returns results at a list.
 
     results.append (termResults)
 
@@ -90,8 +62,6 @@ listPrintLineBreaks (printableResults)
 ##Next step: Right now, the program returns the search
 ##term and a few words next to it. It would be more useful
 ##if I could do the following:
-## (3/4) Find a good way to archive old commentary. Maybe
-## make them a separate text document.
 ##1) Find the headline of the news item in which the search
 ##term appears.
 ##2) Find the publication date. (The tip from @Fichtitious
@@ -102,6 +72,9 @@ listPrintLineBreaks (printableResults)
 ##5) Maybe prompt the user (me, I'm assuming) to enter a
 ##numerical rating of how interesting or relevant the article
 ##was. Save the rating in the dictionary.
+
+##Eventual step: apply some of the more advanced
+##moves @Fichtitious mentioned in the email he sent me.
 
 
 ##Things that might help me with next step #1:
@@ -123,17 +96,8 @@ listPrintLineBreaks (printableResults)
 ##of that article!
 
 
-###Eventual step: apply some of the more advanced
-###moves @Fichtitious mentioned in the email he sent me.
 
 
-##----------------------------------
-##OUTTAKE
-##@Fichtitious recommended the below for my
-##PracticeWebscraper project, but I'm not sure if it
-##will work with the current project.
-##import json
-##forecast = json.load (page)
 
 
 
