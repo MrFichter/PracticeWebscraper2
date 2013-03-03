792350d781b69a2998464783bf8eaca259acc8d8
Jonathan Fichter <jonathan.fichter@live.com>
2013-03-03 10:34:01 -0500

    Giving up on lookaheads. I couldn't get lookaheads to work. Because putting in new lines strategically seems to have worked out, I'm going to quit here rather than continuing to test whether other strategies might also work. I'll conclude, for now, that other strategies might work but would probably take a while to figure out.

--- experimental.py
+++ experimental.py
@@ -1,12 +1,25 @@
 import urllib2 ## There's also urllib, but @Fichtitious \
 ##recommended urllib2.
 
+
 rawPage = urllib2.urlopen ('http://www.dclibrary.org/newsreleases')
 rawContent = rawPage.read()
 
 
 import re
 
+
+
+##LOOKAHEAD STRATEGY
+
+results = re.findall ('<h3 (?=weather)', rawContent)
+
+
+
+
+
+'''
+##NEW LINES AT PARAGRAPH BREAKS STRATEGY
 
 headerNonGreedy = '<h3 class.*?h3'
 headerGreedy = '<h3 class.*h3' ## Yields same results as headerNonGreedy.
@@ -31,7 +44,6 @@ results = re.findall (headerGreedy + '.*' + term + '.*' + '\n', \
 pageWithParagraphBreaks) ##Putting the \ character above does not seem \
 ##to cause any problems.
 
-
-print results
+'''
 
 
